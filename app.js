db251b7e333f3850b8a42cb67df27ea17a16560b
Varun V <varunofficial99@gmail.com>
2020-09-09 13:46:45 +0530

    Allow Cross Origin

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var app = express();
 var cheerio = require('cheerio');
 var request = require('request');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/',function(req,res){
     res.redirect('/canada');
 })
